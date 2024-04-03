1528db4c2e4cbc0a879846a0cadb947ff2e5e23e
DaniAkiode <dannyakiode@hotmail.co.uk>
2024-04-03 22:48:09 +0100

    PLayer names

--- Web-Scrapper/WebScrapper.py
+++ Web-Scrapper/WebScrapper.py
@@ -7,10 +7,14 @@ response = requests.get(url)
 
 soup = BeautifulSoup(response.text, 'html.parser')
 
-links = soup.find_all('a')
+#links = soup.find_all('a')
 
-for link in links:
-    print(link.get('href'))
+#for link in links:
+#    print(link.get('href'))
 
 
+player_names = soup.find_all('a')
+print
+for player_name in player_names:
+    print(player_name.get('title'))
 #https://chat.openai.com/share/6ec9149a-b6e1-4dd8-943a-de60ff070611
